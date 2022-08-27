3f5705b10cf7eb83eb904b654176f387851569e9
Dchoi0729 <yc3878@columbia.edu>
2022-08-27 11:54:34 +0000

    complete entry and index page

--- wiki/encyclopedia/views.py
+++ wiki/encyclopedia/views.py
@@ -2,9 +2,25 @@ from django.shortcuts import render
 
 from . import util
 
+import markdown
 
 def index(request):
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
     })
 
+
+def entry(request, name):
+    entry = util.get_entry(name)
+    if entry:
+        return render(request, "encyclopedia/entry.html", {
+            "name" : name,
+            "entry" : markdown.markdown(entry)
+        })
+    else:
+        return render(request, "encyclopedia/apology.html", {
+            "message" : f"Sorry...but a \"{name}\" page does not exist"
+        })
+
+def search(request):
+    return render(request, "encyclopedia/search.html")
