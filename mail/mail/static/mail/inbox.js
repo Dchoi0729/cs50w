b527fcc96c08c9a787e791e43d88896d7a7752b3
Dchoi0729 <yc3878@columbia.edu>
2022-09-09 05:49:51 +0000

    compose emails

--- mail/mail/static/mail/inbox.js
+++ mail/mail/static/mail/inbox.js
@@ -1,4 +1,16 @@
 document.addEventListener('DOMContentLoaded', function() {
+  // Make sure static js is working
+  console.log("hi")
+
+  // Test to see emails
+  fetch('/emails/inbox')
+  .then(response => response.json())
+  .then(emails => {
+      // Print emails
+      console.log(emails);
+
+      // ... do something else with emails ...
+  });
 
   // Use buttons to toggle between views
   document.querySelector('#inbox').addEventListener('click', () => load_mailbox('inbox'));
@@ -8,6 +20,35 @@ document.addEventListener('DOMContentLoaded', function() {
 
   // By default, load the inbox
   load_mailbox('inbox');
+
+  // Compose and send email
+  document.getElementById("compose-form").onsubmit = () => {
+    const recipients = document.getElementById("compose-recipients").value;
+    const subject = document.getElementById("compose-subject").value;
+    const body = document.getElementById("compose-body").value;
+
+    fetch('/emails', {
+      method: 'POST',
+      body: JSON.stringify({
+        recipients: recipients,
+        subject: subject, 
+        body: body
+      })
+    })
+    .then(response => response.json())
+    .then(result => {
+      console.log(result);
+      if(!result["error"]){
+        load_mailbox('sent');
+      }else{
+        const element = document.createElement('li');
+        element.innerHTML = result["error"];
+        document.getElementById("compose-error").append(element);
+      }
+    })
+    return false
+  }
+
 });
 
 function compose_email() {
@@ -30,4 +71,7 @@ function load_mailbox(mailbox) {
 
   // Show the mailbox name
   document.querySelector('#emails-view').innerHTML = `<h3>${mailbox.charAt(0).toUpperCase() + mailbox.slice(1)}</h3>`;
+  
+  // TODO: 2 have a switch case here
+  //    render email by create a clickable div with onclick event handler that triggers put fetch request
 }
